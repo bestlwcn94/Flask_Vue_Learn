906c0ffc0f2baf6adfca7dbe72d2f1c917e2b090
wewe <simplelly99z@outlook.com>
2023-08-22 02:01:39 +0800

    第三次提交，删除之前不需要引入的naive-ui

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index"
 import ElementPlus from 'element-plus'
-import naive from 'naive-ui'
 import "./style.css"
 import 'element-plus/dist/index.css'
 createApp(App).use(ElementPlus).use(naive).use(router).mount('#app')
